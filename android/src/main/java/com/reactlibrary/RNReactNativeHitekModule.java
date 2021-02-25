585014206f641db22d32b6ad8e5d556c36e8be8d
duonghaidang <dangapagdang@gmail.com>
2021-02-25 15:49:04 +0700

    update

--- android/src/main/java/com/reactlibrary/RNReactNativeHitekModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeHitekModule.java
@@ -1,11 +1,16 @@
 
 package com.reactlibrary;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.re  act.modules.core.DeviceEventManagerModule;
+
 import android.util.Log;
 
 import java.net.InetAddress;
@@ -14,20 +19,45 @@ import java.util.Enumeration;
 
 public class RNReactNativeHitekModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNReactNativeHitekModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @ReactMethod
-    public void checkPrimeNumber(int  number){
-      Log.d("Dang ne", "May ra string ko " + number);
+    public RNReactNativeHitekModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
     }
 
-  @Override
-  public String getName() {
-    return "RNReactNativeHitek";
-  }
+    @ReactMethod
+    public void checkPrimeNumber(int number) {
+        Log.d("Dang ne", "May ra string ko " + number);
+        int i, m = 0, flag = 0;
+        int n = number;//it is the number to be checked
+        m = n / 2;
+        if (n == 0 || n == 1) {
+            System.out.println(n + " is not prime number");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    System.out.println(n + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                WritableMap params = Arguments.createMap();
+                params.putInt("prime", number);
+                sendEvent(reactContext, "PRIME_KEY", params);
+                System.out.println(n + " is prime number");
+            }
+
+        }
+    }
+
+    private void sendEvent(ReactContext reactContext, String eventName, WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+    }
+
+    @Override
+    public String getName() {
+        return "RNReactNativeHitek";
+    }
 }
